f81957ac2231a09dd67245a0152ab6767aebb251
Hediyeh-Eshaqi <hediyeh.eshaqi@gmail.com>
2022-01-17 13:09:40 +0330

    Working on cruds and some other changes

--- Program.cs
+++ Program.cs
@@ -7,19 +7,19 @@ namespace project
     class disease
     {
         string name;
-        List<string> daroomos = new List<string>();
-        List<string> darooman = new List<string>();
-        public Dictionary<string, string> drugsEffects = new Dictionary<string, string>();
-        public Dictionary<string, char> alergies = new Dictionary<string, char>();
+        public List<string> daroomos = new List<string>();
+        public List<string> darooman = new List<string>();
+        // public Dictionary<string, string> drugsEffects = new Dictionary<string, string>();
+        // public Dictionary<string, char> alergies = new Dictionary<string, char>();
         public disease(string name)
         {
             this.name = name;
         }
-        public void adddrug(drug a)
-        {
-            //daroo.Add(a);
-            ///
-        }
+        // public void adddrug(drug a)
+        // {
+        //     //daroo.Add(a);
+        //     ///
+        // }
         public void adddrugeffect(string drugef)
         {
             string[] spil = drugef.Split(",");
@@ -27,10 +27,26 @@ namespace project
             if (spil[1].Contains("+") == true)
             {
                 daroomos.Add(drugef);
+                string[] drugname = spil[0].Split('(');
+                
+                long hash = drugStore.polynomialRollingHash(drugname[1])%2000;
+                if (hash<0)
+                {
+                    hash*=-1;
+                }
+                drugStore.hash_table[hash][drugname[1]].goodfor.Add(this.name);
             }
             if (spil[1].Contains("-") == true)
             {
                 darooman.Add(drugef);
+                string[] drugname = spil[0].Split('(');
+
+                long hash = drugStore.polynomialRollingHash(drugname[1])%2000;
+                if (hash<0)
+                {
+                    hash*=-1;
+                }
+                drugStore.hash_table[hash][drugname[1]].badfor.Add(this.name);
             }
         }
     }
@@ -39,7 +55,9 @@ namespace project
         public string name;
         public int price;
         public Dictionary<string, string> drugsEffects = new Dictionary<string, string>();
-        public Dictionary<string, char> alergies = new Dictionary<string, char>();
+        // public Dictionary<string, char> alergies = new Dictionary<string, char>();
+        public List<string> goodfor = new List<string>();
+        public List<string> badfor = new List<string>();
         public drug(string name, int price)
         {
             this.name = name;
@@ -49,15 +67,18 @@ namespace project
         {
             drugsEffects.Add(drugName, Effect);
         }
-        public void addAlergy(string allergy, char Effect)
-        {
-            alergies.Add(allergy, Effect);
-        }
+        // public void addAlergy(string allergy, char Effect)
+        // {
+        //     alergies.Add(allergy, Effect);
+        // }
     }
 
 
     class drugStore
     {
+        public static List<string> random_drug= new List<string>();
+        public static List<string> random_disease=new List<string>();
+        public static List<string> random_effect = new List<string>();
         public static long polynomialRollingHash(String str)
         {
             // P and M
@@ -95,6 +116,9 @@ namespace project
         }
         public void init()
         {
+            int random_dc =0;
+            int random_dsc=0;
+            int random_ec=0;
             for (int i = 0; i < 2000; i++)
             {
                 hash_table[i] = new Dictionary<string, drug>();
@@ -103,12 +127,18 @@ namespace project
             foreach (string line in lines)
             {
                 string[] splited = line.Split(':');
-                long hash = polynomialRollingHash(splited[0]) % 2000;
+                long hash = polynomialRollingHash(splited[0].Remove(splited[0].Length - 1, 1)) % 2000;
+                if (random_dc<15)
+                {
+                    random_drug.Add(splited[0].Remove(splited[0].Length - 1, 1));
+                    random_dc++;
+                }
+                
                 if (hash < 0)
                 {
                     hash *= -1;
                 }
-                hash_table[hash].Add(splited[0], new drug(splited[0], int.Parse(splited[1])));
+                hash_table[hash].Add(splited[0].Remove(splited[0].Length - 1, 1), new drug(splited[0].Remove(splited[0].Length - 1, 1), int.Parse(splited[1])));
             }
             ///////////////////////////////////////////////////////////////////////////////////
             for (int i = 0; i < 500; i++)
@@ -119,8 +149,13 @@ namespace project
             foreach (string line in lines1)
             {
                 string[] splited = line.Split(':');
-                long hash = polynomialRollingHash(splited[0].Trim()) % 500;
-                disease a = new disease(splited[0]);
+                long hash = polynomialRollingHash(splited[0].Remove(splited[0].Length - 1, 1)) % 500;
+                disease a = new disease(splited[0].Remove(splited[0].Length - 1, 1));
+                if (random_dsc<15)
+                {
+                    random_disease.Add(splited[0].Remove(splited[0].Length - 1, 1));
+                    random_dc++;
+                }
                 if (hash < 0)
                 {
                     hash *= -1;
@@ -132,7 +167,32 @@ namespace project
                     //how hash q?
                     ///trim q?
                 }
-                //hash_table[hash].Add(splited[0], new drug(splited[0], int.Parse(splited[1])));
+                hash_table2[hash].Add(splited[0].Remove(splited[0].Length - 1, 1), a);
+            }
+            /////////////////////////////////////////////////////// eeffeecctt
+            string[] lines2 = System.IO.File.ReadAllLines("effects.txt");
+            foreach (string line in lines2)
+            {
+                string[] splited = line.Split(':');
+                long hash = polynomialRollingHash(splited[0].Remove(splited[0].Length - 1, 1)) % 2000;
+               
+                if (hash < 0)
+                {
+                    hash *= -1;
+                }
+                string[] splited2 = splited[1].Split(";");
+                for (int i = 0; i < splited2.Length; i++)
+                {
+                    string[] newstr = splited2[i].Split('(');
+                    string[] newstr1 = newstr[1].Split(')');
+                    string[] final = newstr1[0].Split(',');
+                    hash_table[hash][splited[0].Remove(splited[0].Length - 1, 1)].addEffect(final[0],final[1]);
+                    if (random_ec<15)
+                    {
+                        random_effect.Add(final[1]);
+                        random_ec++;
+                    }
+                }
             }
 
         }
@@ -144,7 +204,7 @@ namespace project
                 hash *= -1;
             }
 
-            if (hash_table[hash].ContainsKey(diseasename) == true)
+            if (hash_table2[hash].ContainsKey(diseasename) == true)
             {
                 throw new Exception("this disease is already exist!! you can not add it again!!");
             }
@@ -175,12 +235,19 @@ namespace project
             else
             {
                 hash_table[hash].Add(name, new drug(name, price));
-                //add the new drug to our dataset
 
-                File.AppendAllText("drugs.txt", name + " : " + price);
                 //some random drugs for this drug effect
+                Random rnd = new Random();
 
-
+                int d = rnd.Next(0, 16); // returns random integers >= 0 and < 16
+                int e = rnd.Next(0, 16);
+                long hash_d = polynomialRollingHash(random_drug[d]) % 2000;
+                if (hash_d < 0)
+                {
+                    hash_d *= -1;
+                }
+                hash_table[hash_d][random_drug[d]].addEffect(name,random_effect[e]);
+                hash_table[hash][name].addEffect(random_drug[d],random_effect[e]);
 
 
                 //some random alergy for this drug alergy
@@ -199,11 +266,20 @@ namespace project
                 //Console.Write(hash_table2[hash][diseasename].alergies + " : " + hash_table2[hash][diseasename].drugsEffects);
 
 
-                Console.WriteLine("these drugs effects:");
-                foreach (KeyValuePair<string, string> item in hash_table2[hash][diseasename].drugsEffects)
-                {
-                    Console.WriteLine(item.Key, item.Value);
+                Console.WriteLine("daroo haii ke roye in bimari asare mostbat darand:");
+                // foreach (KeyValuePair<string, string> item in hash_table2[hash][diseasename].drugsEffects)
+                // {
+                //     Console.WriteLine(item.Key, item.Value);
+                // }
+                foreach(string m in hash_table2[hash][diseasename].daroomos){
+                    Console.Write(m+" ");
                 }
+                Console.WriteLine();
+                Console.WriteLine("daroo haii ke roye in bimari asare manfi darand:");
+                foreach(string m in hash_table2[hash][diseasename].darooman){
+                    Console.Write(m+" ");
+                }
+                Console.WriteLine();
             }
             else
             {
@@ -241,15 +317,22 @@ namespace project
             if (hash_table[hash].ContainsKey(name) == true)
             {
                 Console.WriteLine(hash_table[hash][name].name + " : " + hash_table[hash][name].price);
+                Console.WriteLine("this drug is good for:");
+                foreach (string item in hash_table[hash][name].goodfor)
+                {
+                    Console.Write(item+" ");
+                }
+                Console.WriteLine();
+                Console.WriteLine("this drug is bad for:");
+                foreach (string item in hash_table[hash][name].badfor)
+                {
+                    Console.Write(item+" ");
+                }
+                Console.WriteLine();
                 Console.WriteLine("this drug effects are:");
                 foreach (KeyValuePair<string, string> item in hash_table[hash][name].drugsEffects)
                 {
-                    Console.WriteLine(item.Key, item.Value);
-                }
-                Console.WriteLine("this drug alergies are:");
-                foreach (KeyValuePair<string, char> item in hash_table[hash][name].alergies)
-                {
-                    Console.WriteLine(item.Key, item.Value);
+                    Console.WriteLine("[ "+item.Key+" , " +item.Value+" ]");
                 }
             }
             else
@@ -272,47 +355,77 @@ namespace project
 
             if (flag == 1)
             {
-                drug test = new drug("test", 1000);
-                Console.WriteLine(test.name);
-                test.addAlergy("alergy", '+');
-                test.addEffect("drug", "effect");
-                Console.WriteLine(test.drugsEffects["drug"]);
-                Console.WriteLine(test.alergies["alergy"]);
-
                 d.init();
-
-                long hash = drugStore.polynomialRollingHash("Drug_cwfordbcup" + " ") % 2000;
-                if (hash < 0)
-                {
-                    hash *= -1;
-                }
-                Console.WriteLine(hash);
-                Console.WriteLine(drugStore.hash_table[hash]["Drug_cwfordbcup" + " "].price);
-
             }
             else if (flag == 2)
             {
-                d.addNewDrug("test", 10000);
+                d.readDisease("Dis_ebpmvoxdhl");
+                d.deleteDisease("Dis_ebpmvoxdhl");
+                d.readDisease("Dis_ebpmvoxdhl");
+                //tadakhole darooii
             }
             else if (flag == 3)
             {
-                d.readDrug("test");
+                //tadakhole darooii va bimari
             }
             else if (flag == 4)
             {
+                //hazine yek noskhe daroo
 
             }
             else if (flag == 5)
             {
+                //afzayesh gheimate daroo ha
 
             }
             else if (flag == 6)
             {
+                //taghirat rooye sakhtar dade
+                d.addNewDrug("testdrug",8000);
 
             }
             else if (flag == 7)
             {
+                //search dar sakhtar dade
+                int option=-1;
+                
+                
+                while(option != 3){
+                    try
+                    {
+                        Console.WriteLine("Enter your desired option:".PadLeft(27, ' '));
+                        Console.WriteLine("1.Search a drug".PadLeft(22, ' '));
+                        Console.WriteLine("2.Search a dises".PadLeft(23, ' '));
+                        Console.WriteLine("3.Back to menue".PadLeft(22, ' '));
+                        option = int.Parse(Console.ReadLine());
+                        Console.Clear();
+                    }
+                    catch (System.Exception)
+                    {
+                        Console.Clear();
+                        Console.ForegroundColor = ConsoleColor.Red;
 
+                        Console.WriteLine("Invalid input!! your input is not a number!! Enter it again!");
+                        Console.ForegroundColor = ConsoleColor.White;
+                        continue;
+                    }
+                    if(option==1){
+                        string drug_name = Console.ReadLine();
+                        d.readDrug(drug_name);
+                    }
+                    else if(option==2){
+
+                    }
+                    else if(option==3){
+
+                    }
+                    else{
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("Your input is not in the correct range!! Enter it again!");
+                        Console.ForegroundColor = ConsoleColor.White;
+                    }
+                }
+                
             }
             else
             {
